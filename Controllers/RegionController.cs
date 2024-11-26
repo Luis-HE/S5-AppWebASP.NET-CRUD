399e553080bd0100da07dced1328fd8bbe313f7a
Luis Hilario <faustino.leon.contractor@bbva.com>
2024-11-25 23:16:18 -0500

    Despliegue Inicial V.0.1

---
+++ Controllers/RegionController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication_S5_CRUD.Models;
+
+namespace WebApplication_S5_CRUD.Controllers
+{
+    public class RegionController : Controller
+    {
+        // GET: Region
+        public ActionResult Index()
+        {
+            using (masterEntities contexto = new masterEntities())
+            {
+                return View(contexto.Region.ToList());
+            }
+        }
+
+        // GET: Region/Details/5
+        public ActionResult Details(int id)
+        {
+            using (masterEntities contexto = new masterEntities())
+            {
+                return View(contexto.Region.Where(x => x.RegionID == id).FirstOrDefault());
+            }
+        }
+
+        // GET: Region/Create
+        public ActionResult Create()
+        {
+            using (masterEntities contexto = new masterEntities())
+            {
+                return View();
+            }
+        }
+
+        // POST: Region/Create
+        [HttpPost]
+        public ActionResult Create(Region region)
+        {
+            try
+            {
+                using (masterEntities contexto = new masterEntities())
+                {
+                    // TODO: Add insert logic here
+                    var newRegion = new Region
+                    {
+                        RegionID = contexto.Region.Max(r=>r.RegionID)+1,
+                        RegionDescription = region.RegionDescription
+                    };
+                    contexto.Region.Add(newRegion);
+                    contexto.SaveChanges();
+                    return RedirectToAction("/Index");
+
+                }                
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Region/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (masterEntities contexto = new masterEntities())
+            {
+                return View(contexto.Region.Where(x => x.RegionID == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Region/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Region regionArray)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                using (masterEntities contexto = new masterEntities())
+                {
+                    contexto.Entry(regionArray).State = (System.Data.Entity.EntityState)EntityState.Modified;
+                    contexto.SaveChanges();
+                    return RedirectToAction("/Index");
+                }
+            }
+            catch
+            {
+                return View("/Index");
+            }
+        }
+
+        // GET: Region/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (masterEntities contexto = new masterEntities())
+            {
+                return View(contexto.Region.Where(x => x.RegionID == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Region/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                using (masterEntities contexto = new masterEntities())
+                {
+                    Region objRegion = contexto.Region.Where(x => x.RegionID == id).FirstOrDefault();
+                    contexto.Region.Remove(objRegion);
+                    contexto.SaveChanges();
+                }
+                return RedirectToAction("/Index");
+            }
+            catch
+            {
+                return View("/");
+            }
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication_S5_CRUD.MvcApplication" Language="C#" %>
